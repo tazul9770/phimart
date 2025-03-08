bf0789753a1cb85ca18e60bfac4656f542334cb3
Tazul Islam <linkedin52425@gmail.com>
2025-03-08 14:39:20 +0600

    again ready

--- product/views.py
+++ product/views.py
@@ -19,7 +19,6 @@ class ProductViewSet(ModelViewSet):
      - Support searching by name, description, and category
      - Support ordering by price and updated_at
     """
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_class = ProductFilter
@@ -27,6 +26,9 @@ class ProductViewSet(ModelViewSet):
     search_fields = ['name', 'description']
     ordering_fields = ['price', 'updated_at']
     permission_classes = [IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        return Product.objects.prefetch_related('images').all()
 
     @swagger_auto_schema(
         operation_summary='Retrive a list of products'
