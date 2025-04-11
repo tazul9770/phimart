21996a15f43ac41fa3f4b75dc2123fc3a6658017
Tazul Islam <linkedin52425@gmail.com>
2025-04-11 22:19:25 +0600

    new update done

--- phimart/settings.py
+++ phimart/settings.py
@@ -201,7 +201,7 @@ SWAGGER_SETTINGS = {
 }
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_HOST = config('EMAIL_HOST', default='localhost')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
 EMAIL_PORT = config('EMAIL_PORT')
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
